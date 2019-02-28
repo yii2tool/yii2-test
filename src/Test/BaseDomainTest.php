5a15b82c68b5e57fb67d99cc07193f5c2fa31b4e
Vitaliy <theyamshikov@yandex.ru>
2019-02-28 20:28:13 +0600

    make base test

---
+++ src/Test/BaseDomainTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace yii2lab\test\Test;
+
+use yii2lab\test\Test\Unit;
+use yii\data\DataProviderInterface;
+use yii\helpers\ArrayHelper;
+use yii2lab\test\helpers\DataHelper;
+
+class BaseDomainTest extends Unit
+{
+
+    public $package;
+
+    public function assertDataProvider(DataProviderInterface $dataProvider, $method) {
+        $collection = $dataProvider->getModels();
+        $pagination = $dataProvider->getPagination();
+        $actual = [
+            'pagination' => [
+                'page' => $pagination->page,
+                'pageCount' => $pagination->pageCount,
+                'pageSize' => $pagination->pageSize,
+                'totalCount' => $pagination->totalCount,
+                'limit' => $pagination->limit,
+                'offset' => $pagination->offset,
+            ],
+            'collection' => ArrayHelper::toArray($collection),
+        ];
+        $expect = DataHelper::loadForTest2($this->package, $method . 'DataProvider', $actual);
+        $this->tester->assertEquals($expect, $actual);
+    }
+
+}
