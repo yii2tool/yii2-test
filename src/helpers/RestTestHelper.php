06541e485ce61bd4f93092b7aa9fac19bcc538d1
Vitaliy <theyamshikov@yandex.ru>
2019-05-07 19:25:19 +0600

    fix: undo refacto

--- src/helpers/RestTestHelper.php
+++ src/helpers/RestTestHelper.php
@@ -17,7 +17,7 @@ use yii2rails\app\domain\helpers\EnvService;
 use yii2rails\extension\web\enums\HttpMethodEnum;
 use yii2rails\extension\yii\helpers\FileHelper;
 use yii2module\account\domain\v3\entities\LoginEntity;
-use yii2module\account\domain\v3\helpers\test\AuthTestHelper;
+use yubundle\account\domain\v2\helpers\test\AuthTestHelper;
 
 class RestTestHelper {
 
