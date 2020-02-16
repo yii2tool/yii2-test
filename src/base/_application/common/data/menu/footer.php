b3c916f7af4159e763305323faabe9c612cd36b5
Vitaliy <theyamshikov@yandex.ru>
2020-02-16 15:39:25 +0600

    refactor: change domain url env name

--- src/base/_application/common/data/menu/footer.php
+++ src/base/_application/common/data/menu/footer.php
@@ -20,7 +20,7 @@ return [
 		],
 		[
 			'label' => ['main', 'go_to_backend'],
-			'url' => $_ENV['ADMIN_DOMAIN_URL'],
+			'url' => $_ENV['ADMIN_URL'],
 			'access' => ApplicationPermissionEnum::BACKEND_ALL,
 			'visible' => APP != BACKEND,
 		],
